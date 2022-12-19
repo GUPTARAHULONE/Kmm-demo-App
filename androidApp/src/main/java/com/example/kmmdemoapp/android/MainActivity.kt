bb8eb23714b5ef983ed8804ec378369a07912eb0
rahulgu <guptarahul26318@gmail.com>
2022-12-19 23:13:31 +0530

    added edit text and textview in compose

--- androidApp/src/main/java/com/example/kmmdemoapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/kmmdemoapp/android/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.kmmdemoapp.android
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.kmmdemoapp.Greeting
@@ -30,30 +33,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingView() {
-    var input1 by rememberSaveable { mutableStateOf("0") }
-    var input2 by rememberSaveable { mutableStateOf("0") }
+    var input1 by remember { mutableStateOf("") }
+    var input2 by remember { mutableStateOf("") }
 
 
-    Column (modifier = Modifier
-        .fillMaxWidth()
-        .padding(60.dp)){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(60.dp)
+    ) {
 
-        OutlinedTextField(value = input1,
+        OutlinedTextField(
+            value = input1,
             onValueChange = { value ->
                 input1 = value
             },
+            label = { Text("Enter First Value") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
         )
 
 
         Spacer(modifier = Modifier.padding(top = 20.dp))
 
-        OutlinedTextField(value = input2,
+        OutlinedTextField(
+            value = input2,
             onValueChange = { value ->
                 input2 = value
-            })
-        Spacer(modifier = Modifier.padding(top = 40.dp))
+            },
+            label = { Text("Enter Second Value") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
+        )
+        Spacer(modifier = Modifier.padding(top = 50.dp))
 
-        Text(text = Greeting().add(input1, input2))
+
+
+        Text(
+            text = "Answer= " + Greeting().add(input1, input2),
+            modifier = Modifier.padding(20.dp))
     }
 
 }
@@ -62,6 +78,6 @@ fun GreetingView() {
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-      //  GreetingView("Hello, Android!")
+        //  GreetingView("Hello, Android!")
     }
 }
